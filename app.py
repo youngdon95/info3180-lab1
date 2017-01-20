1c60edab51776baaebe3bb3a62a3ccd9e0c1eaff
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2017-01-20 18:14:57 -0500

    info3180 lab 1 starter code initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+'''
+# Routing for your application.
+'''
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=8080)
